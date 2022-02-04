88e728a6cb8f82ec726605839dd44d7f48fa574a
dolsol3 <dolsol4@gmail.com>
2022-02-04 16:07:38 +0900

    자잘한 ui 개선
    만들기 화면 넘어가는 부분 수정
    프로그레스바 오류 수정하기전

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,17 +1,36 @@
-// mbti만들기
+// 상태표시줄 url 가져오기
+var 쿼리스트링2 = new URLSearchParams(window.location.search);
+// 로고
 $(".nav-logo").click(function () {
-  location.href = "/index.html";
+  location.href = "../index.html";
 });
-// 사용가이드
+// mbti만들기
+
+// 사용가이드 일단 숨김
+$(".nav-guide").hide();
 // 회원가입 & 로그인
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     $(".nav-mypage").show();
     $(".nav-join").hide();
+    $(".nav-logout").show();
+    $(".nav-make").click(function () {
+      location.href = "../making/making1.html";
+    });
   } else {
     $(".nav-mypage").hide();
+    $(".nav-logout").hide();
+    $(".nav-make").click(function () {
+      alert("로그인을 해야 이용할 수 있습니다");
+      location.href = "../login.html";
+    });
   }
 });
 // 내화면
-
+$(".nav-mypage").click(function () {
+  alert("개인 화면 준비중입니다");
+});
 // 문의하기
+$(".nav-contact").click(function () {
+  alert("광고 및 제휴 문의 : dolsol4@gmail.com");
+});
